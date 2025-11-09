580c30937a7075ba1ffcdfd5bd110dda9880da54
saybube <ebubenwamaka@gmail.com>
2025-11-09 02:49:19 +0100

    code prints msgs

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+let first_line = "C is fun";
+let second_line = "Python is cool";
+let third_line = "JavaScript is amazing"
+
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
